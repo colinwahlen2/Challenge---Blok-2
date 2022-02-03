88398e3981a6a878727e6d283f45dab7d424118e
colinwahlen2 <90839957+colinwahlen2@users.noreply.github.com>
2022-02-03 17:49:14 +0100

    wijzigingen

---
+++ script.js
@@ -0,0 +1,11 @@
+function blok1() {
+    document.getElementById("blok1").style.display = "block";
+}
+
+function wordpress() {
+    document.getElementById("wordpress").style.display = "block";
+}
+
+function python() {
+    document.getElementById("python").style.display = "block";
+}
